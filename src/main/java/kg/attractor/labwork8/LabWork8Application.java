3ca8ad9017ec1ca967362028ba4fcfc11c87b070
Askar <nurbekovaskar099@gmail.com>
2026-03-31 23:42:39 +0600

    Starting pack

---
+++ src/main/java/kg/attractor/labwork8/LabWork8Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.labwork8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWork8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWork8Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/attractor/labwork8/LabWork8ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.labwork8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWork8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
