26152e10530ad095918421a39a8f9a30298d2fb6
Askar <nurbekovaskar099@gmail.com>
2026-04-01 21:49:56 +0600

    Created controller classes

---
+++ src/main/java/kg/attractor/labwork8/controller/AuthController.java
@@ -0,0 +1,16 @@
+package kg.attractor.labwork8.controller;
+
+import kg.attractor.labwork8.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/register")
+@RequiredArgsConstructor
+public class AuthController {
+
+}
