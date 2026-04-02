351d063c4efde199ccfe9e96d748358b452e54c8
Askar <nurbekovaskar099@gmail.com>
2026-04-02 18:23:50 +0600

    removed not useble annotations

--- src/main/java/kg/attractor/labwork8/dto/AnswerDto.java
+++ src/main/java/kg/attractor/labwork8/dto/AnswerDto.java
@@ -1,14 +1,10 @@
 package kg.attractor.labwork8.dto;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class AnswerDto {
     @NotNull
