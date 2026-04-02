b654d781093151b8276ec7579a740ac8efc3caff
Askar <nurbekovaskar099@gmail.com>
2026-04-02 18:26:36 +0600

    created service classes

--- src/main/java/kg/attractor/labwork8/service/QuizService.java
+++ src/main/java/kg/attractor/labwork8/service/QuizService.java
@@ -1,4 +1,24 @@
 package kg.attractor.labwork8.service;
 
+import kg.attractor.labwork8.dto.AnswerDto;
+import kg.attractor.labwork8.dto.QuizDto;
+import kg.attractor.labwork8.dto.QuizResultDto;
+import kg.attractor.labwork8.dto.StatisticsDto;
+import kg.attractor.labwork8.exception.AlreadyCompletedException;
+
+import java.util.List;
+import java.util.Map;
+
 public interface QuizService {
+    List<StatisticsDto> getLeaderBoard(Long quizId);
+
+    QuizResultDto getResult(Long quizId);
+
+    Map<Long, String> getAnswers(List<AnswerDto> answersDto, Long userId) throws AlreadyCompletedException;
+
+    QuizDto findById(Long id);
+
+    List<QuizDto> findQuizzes();
+
+    void createQuiz(QuizDto quizDto);
 }
--- src/main/java/kg/attractor/labwork8/service/impl/UserServiceImpl.java
+++ src/main/java/kg/attractor/labwork8/service/impl/UserServiceImpl.java
@@ -1,12 +1,47 @@
 package kg.attractor.labwork8.service.impl;
 
+import kg.attractor.labwork8.dao.QuizDao;
+import kg.attractor.labwork8.dao.UserDao;
+import kg.attractor.labwork8.dto.StatisticsDto;
+import kg.attractor.labwork8.dto.UserDto;
+import kg.attractor.labwork8.exception.NotFoundException;
+import kg.attractor.labwork8.model.User;
 import kg.attractor.labwork8.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    private final PasswordEncoder encoder;
+    private final QuizDao quizDao;
+
+    @Override
+    public StatisticsDto getUserStatistics(long userId) {
+        User user = userDao.findById(userId)
+                .orElseThrow(NotFoundException::new);
+
+        double averageScore = quizDao.findAverageScore(userId);
+
+        return StatisticsDto.builder()
+                .email(user.getEmail())
+                .score(averageScore)
+                .build();
+    }
+
+    @Override
+    public void register(UserDto userDto) {
+        User user =  User
+                .builder()
+                .username(userDto.getUsername())
+                .email(userDto.getEmail())
+                .password(encoder.encode(userDto.getPassword()))
+                .build();
+        userDao.register(user);
+        log.info("User {} registered", userDto.getEmail());
+    }
 }
